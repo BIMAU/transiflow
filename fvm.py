78ddc1b1cca1d4aa170f84d2642e00b9d1508680
Sven Baars <s.baars@rug.nl>
2020-08-31 13:36:14 +0200

    Add a u_yy implementation.

--- fvm.py
+++ fvm.py
@@ -35,3 +35,40 @@ class Derivatives:
             for j in range(ny):
                 for k in range(nz):
                     Derivatives._u_xx(atom[i, j, k, 1, 1, :], k, j, i, z, y, x)
+
+    @staticmethod
+    def _u_yy(atom, i, j, k, x, y, z):
+        # distance between u[j] and u[j-1]
+        dy = (y[j] - y[j-2]) / 2
+        # distance between u[j+1] and u[j]
+        dyp1 = (y[j+1] - y[j-1]) / 2
+        # volume size in the x direction
+        dx = (x[i+1] - x[i-1]) / 2
+        # volume size in the z direction
+        dz = z[k] - z[k-1]
+
+        # second order finite difference
+        atom[0] = 1 / dy * dx * dz
+        atom[2] = 1 / dyp1 * dx * dz
+        atom[1] = -atom[0] + atom[2]
+
+    @staticmethod
+    def u_yy(atom, nx, ny, nz, x, y, z):
+        for i in range(nx):
+            for j in range(ny):
+                for k in range(nz):
+                    Derivatives._u_yy(atom[i, j, k, :, 1, 1], i, j, k, x, y, z)
+
+    @staticmethod
+    def v_xx(atom, nx, ny, nz, x, y, z):
+        for i in range(nx):
+            for j in range(ny):
+                for k in range(nz):
+                    Derivatives._u_yy(atom[i, j, k, 1, :, 1], j, i, k, y, x, z)
+
+    @staticmethod
+    def w_yy(atom, nx, ny, nz, x, y, z):
+        for i in range(nx):
+            for j in range(ny):
+                for k in range(nz):
+                    Derivatives._u_yy(atom[i, j, k, 1, 1, :], k, j, i, z, y, x)
--- test_fvm.py
+++ test_fvm.py
@@ -2,6 +2,9 @@ import numpy
 import fvm
 import pytest
 
+def create_coordinate_vector(dx):
+    return numpy.roll(numpy.arange(-dx, 1+dx, dx), -2)
+
 def test_u_xx():
     nx = 5
     ny = 3
@@ -11,9 +14,9 @@ def test_u_xx():
     dy = 1 / (ny + 1)
     dz = 1 / (nz + 1)
 
-    x = numpy.roll(numpy.arange(-dx, 1+dx, dx), -1)
-    y = numpy.roll(numpy.arange(-dy, 1+dy, dy), -1)
-    z = numpy.roll(numpy.arange(-dz, 1+dz, dz), -1)
+    x = create_coordinate_vector(dx)
+    y = create_coordinate_vector(dy)
+    z = create_coordinate_vector(dz)
 
     atom = numpy.zeros([nx, ny, nz, 3, 3, 3])
     fvm.Derivatives.u_xx(atom, nx, ny, nz, x, y, z)
@@ -34,9 +37,9 @@ def test_v_yy():
     dy = 1 / (ny + 1)
     dz = 1 / (nz + 1)
 
-    x = numpy.roll(numpy.arange(-dx, 1+dx, dx), -1)
-    y = numpy.roll(numpy.arange(-dy, 1+dy, dy), -1)
-    z = numpy.roll(numpy.arange(-dz, 1+dz, dz), -1)
+    x = create_coordinate_vector(dx)
+    y = create_coordinate_vector(dy)
+    z = create_coordinate_vector(dz)
 
     atom = numpy.zeros([nx, ny, nz, 3, 3, 3])
     fvm.Derivatives.v_yy(atom, nx, ny, nz, x, y, z)
@@ -57,9 +60,9 @@ def test_w_zz():
     dy = 1 / (ny + 1)
     dz = 1 / (nz + 1)
 
-    x = numpy.roll(numpy.arange(-dx, 1+dx, dx), -1)
-    y = numpy.roll(numpy.arange(-dy, 1+dy, dy), -1)
-    z = numpy.roll(numpy.arange(-dz, 1+dz, dz), -1)
+    x = create_coordinate_vector(dx)
+    y = create_coordinate_vector(dy)
+    z = create_coordinate_vector(dz)
 
     atom = numpy.zeros([nx, ny, nz, 3, 3, 3])
     fvm.Derivatives.w_zz(atom, nx, ny, nz, x, y, z)
@@ -70,3 +73,72 @@ def test_w_zz():
                 print(i, j, k)
                 assert atom[i, j, k, 1, 1, 0] == pytest.approx(1 / dz * dy * dx)
                 assert atom[i, j, k, 1, 1, 2] == pytest.approx(1 / dz * dy * dx)
+
+def test_u_yy():
+    nx = 5
+    ny = 3
+    nz = 2
+
+    dx = 1 / (nx + 1)
+    dy = 1 / (ny + 1)
+    dz = 1 / (nz + 1)
+
+    x = create_coordinate_vector(dx)
+    y = create_coordinate_vector(dy)
+    z = create_coordinate_vector(dz)
+
+    atom = numpy.zeros([nx, ny, nz, 3, 3, 3])
+    fvm.Derivatives.u_yy(atom, nx, ny, nz, x, y, z)
+
+    for i in range(nx):
+        for j in range(ny):
+            for k in range(nz):
+                print(i, j, k)
+                assert atom[i, j, k, 0, 1, 1] == pytest.approx(1 / dy * dx * dz)
+                assert atom[i, j, k, 2, 1, 1] == pytest.approx(1 / dy * dx * dz)
+
+def test_v_xx():
+    nx = 5
+    ny = 3
+    nz = 2
+
+    dx = 1 / (nx + 1)
+    dy = 1 / (ny + 1)
+    dz = 1 / (nz + 1)
+
+    x = create_coordinate_vector(dx)
+    y = create_coordinate_vector(dy)
+    z = create_coordinate_vector(dz)
+
+    atom = numpy.zeros([nx, ny, nz, 3, 3, 3])
+    fvm.Derivatives.v_xx(atom, nx, ny, nz, x, y, z)
+
+    for i in range(nx):
+        for j in range(ny):
+            for k in range(nz):
+                print(i, j, k)
+                assert atom[i, j, k, 1, 0, 1] == pytest.approx(1 / dx * dy * dz)
+                assert atom[i, j, k, 1, 2, 1] == pytest.approx(1 / dx * dy * dz)
+
+def test_w_yy():
+    nx = 5
+    ny = 3
+    nz = 2
+
+    dx = 1 / (nx + 1)
+    dy = 1 / (ny + 1)
+    dz = 1 / (nz + 1)
+
+    x = create_coordinate_vector(dx)
+    y = create_coordinate_vector(dy)
+    z = create_coordinate_vector(dz)
+
+    atom = numpy.zeros([nx, ny, nz, 3, 3, 3])
+    fvm.Derivatives.w_yy(atom, nx, ny, nz, x, y, z)
+
+    for i in range(nx):
+        for j in range(ny):
+            for k in range(nz):
+                print(i, j, k)
+                assert atom[i, j, k, 1, 1, 0] == pytest.approx(1 / dy * dz * dx)
+                assert atom[i, j, k, 1, 1, 2] == pytest.approx(1 / dy * dz * dx)
