284550ccaefdabae5a8b629e5b9f8419ee933ee6
Sven Baars <s.baars@rug.nl>
2020-10-14 13:27:12 +0200

    Add parallel subdomain map support.

--- HYMLSInterface.py
+++ HYMLSInterface.py
@@ -19,14 +19,30 @@ class Vector(Epetra.Vector):
     def dot(self, other):
         return self.Dot(other)
 
+def ind2sub(nx, ny, nz, idx, dof=1):
+    rem = idx
+    var = rem % dof
+    rem = rem // dof
+    i = rem % nx
+    rem = rem // nx
+    j = rem % ny
+    rem = rem // ny
+    k = rem % nz
+    return (i, j, k, var)
+
+def sub2ind(nx, ny, nz, dof, i, j, k, var):
+    return ((k *ny + j) * nx + i) * dof + var
+
 class Interface(continuation.Interface):
     def __init__(self, comm, params, nx, ny, nz):
         continuation.Interface.__init__(self, nx, ny, nz)
 
-        dof = 4
-        n = nx * ny * nz * dof
+        self.dof = 4
         self.comm = comm
-        self.map = Epetra.Map(n, 0, comm)
+
+        self.partition_domain()
+        self.create_map()
+
         self.params = params
         problem_params = self.params.sublist('Problem')
         problem_params.set('nx', self.nx)
@@ -39,6 +55,85 @@ class Interface(continuation.Interface):
 
         prec_params = self.params.sublist('Preconditioner')
         prec_params.set('Partitioner', 'Skew Cartesian')
+
+    def partition_domain(self):
+        rmin = 1e100
+
+        sx = 1
+        sy = 1
+        sz = 1
+
+        nparts = self.comm.NumProc()
+
+        found = False
+
+        # check all possibilities of splitting the map
+        for t1 in range(1, nparts + 1):
+            for t2 in range(1, nparts // t1 + 1):
+                t3 = nparts // (t1 * t2)
+                if t1 * t2 * t3 == nparts:
+                    nx_loc = self.nx // t1
+                    ny_loc = self.ny // t2
+                    nz_loc = self.nz // t3
+
+                    if nx_loc * t1 != self.nx or ny_loc * t2 != self.ny or nz_loc * t3 != self.nz:
+                        continue
+
+                    r1 = abs(self.nx / t1 - self.ny / t2)
+                    r2 = abs(self.nx / t1 - self.nz / t3)
+                    r3 = abs(self.ny / t2 - self.nz / t3)
+                    r = r1 + r2 + r3
+
+                    if r < rmin:
+                        rmin = r
+                        npx = t1
+                        npy = t2
+                        npz = t3
+                        found = True
+
+        if not found:
+            raise Exception('Could not split %dx%dx%d domain in %d parts.' % (self.nx, self.ny, self.nz, nparts))
+
+        pidx, pidy, pidz, _ = ind2sub(npx, npy, npz, nparts, 1)
+
+        # Compute the local domain size and offset.
+        self.nx_local = self.nx // npx
+        self.ny_local = self.ny // npy
+        self.nz_local = self.nz // npz
+
+        self.nx_offset = self.nx_local * pidx
+        self.ny_offset = self.ny_local * pidy
+        self.nz_offset = self.nz_local * pidz
+
+        # Add ghost nodes to factor out boundary conditions in the interior.
+        if pidx > 0:
+            self.nx_local += 2
+            self.nx_offset -= 2
+        if pidx < npx - 1:
+            self.nx_local += 2
+        if pidy > 0:
+            self.ny_local += 2
+            self.ny_offset -= 2
+        if pidy < npy - 1:
+            self.ny_local += 2
+        if pidz > 0:
+            self.nz_local += 2
+            self.nz_offset -= 2
+        if pidz < npz - 1:
+            self.nz_local += 2
+
+    def create_map(self):
+        local_elements = [0] * self.nx_local * self.ny_local * self.nz_local * self.dof
+
+        pos = 0
+        for k in range(self.nz_offset, self.nz_offset + self.nz_local):
+            for j in range(self.ny_offset, self.ny_offset + self.ny_local):
+                for i in range(self.nx_offset, self.nx_offset + self.nx_local):
+                    for var in range(self.dof):
+                        local_elements[pos] = sub2ind(self.nx, self.ny, self.nz, self.dof, i, j, k, var)
+                        pos += 1
+
+        self.map = Epetra.Map(-1, local_elements, 0, self.comm)
 
     def dirtect_solve(self, jac, rhs):
         A = Epetra.CrsMatrix(Epetra.Copy, self.map, 27)
