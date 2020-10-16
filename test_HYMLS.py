10ad36fa955278c29493b00fda374cdb5509ec14
Sven Baars <s.baars@rug.nl>
2020-10-16 10:42:07 +0200

    Add plotting for parallel vectors.

--- test_HYMLS.py
+++ test_HYMLS.py
@@ -1,4 +1,17 @@
 import continuation
+import plot_utils
+
+def gather(x):
+    from PyTrilinos import Epetra
+
+    local_elements = []
+    if x.Comm().MyPID() == 0:
+        local_elements = range(x.Map().NumGlobalElements())
+    local_map = Epetra.Map(-1, local_elements, 0, x.Comm())
+    importer = Epetra.Import(local_map, x.Map())
+    out = Epetra.Vector(local_map)
+    out.Import(x, importer, Epetra.Insert)
+    return out
 
 def test_HYMLS():
     try:
@@ -8,6 +21,7 @@ def test_HYMLS():
     except ImportError:
         return
 
+    dof = 4
     nx = 4
     ny = nx
     nz = nx
@@ -32,6 +46,13 @@ def test_HYMLS():
     x = continuation.continuation(interface, x0, l, target, ds, maxit)
 
     assert x.Norm2() > 0
+
+    # x = gather(x)
+    # if comm.MyPID() == 0:
+    #     print(x)
+
+    #     x = plot_utils.get_state_mtx(x, nx, ny, nz, dof)
+    #     plot_utils.plot_state(x[:,ny//2,:,0], x[:,ny//2,:,2], nx, nz)
 
 if __name__ == '__main__':
     test_HYMLS()
