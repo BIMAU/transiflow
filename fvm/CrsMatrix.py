8149d19a72f6f117b186c6534f022673b676c9c2
Sven Baars <s.baars@rug.nl>
2020-11-27 12:18:25 +0100

    Always remove duplicate and zero entries from the CrsMatrix upon construction.

--- fvm/CrsMatrix.py
+++ fvm/CrsMatrix.py
@@ -1,5 +1,30 @@
+import numpy
+
 class CrsMatrix:
     def __init__(self, coA=None, jcoA=None, begA=None):
         self.coA = coA
         self.jcoA = jcoA
         self.begA = begA
+
+        self.compress()
+
+    def compress(self):
+        ''' Remove zeros and merge duplicate entries, which may occur in the case of periodic
+        boundary conditions.'''
+        idx = 0
+        beg = self.begA[0]
+        for i in range(len(self.begA) - 1):
+            unique_indices, inverse_indices = numpy.unique(self.jcoA[beg:self.begA[i+1]], return_inverse=True)
+
+            values = numpy.zeros(len(unique_indices))
+            for orig_idx, inverse_idx in enumerate(inverse_indices):
+                values[inverse_idx] += self.coA[beg + orig_idx]
+
+            for j in range(len(unique_indices)):
+                if abs(values[j]) > 1e-14:
+                    self.jcoA[idx] = unique_indices[j]
+                    self.coA[idx] = values[j]
+                    idx += 1
+
+            beg = self.begA[i+1]
+            self.begA[i+1] = idx
