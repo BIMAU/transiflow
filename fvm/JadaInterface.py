487a67c44a8f4377645d1ae235df790cf4dffb04
Sven Baars <s.baars@rug.nl>
2021-03-08 11:55:49 +0100

    test: Add some more JaDa tests.

--- fvm/JadaInterface.py
+++ fvm/JadaInterface.py
@@ -20,9 +20,10 @@ class JadaOp:
         return self.mat * x
 
 class JadaPrecOp(object):
-    def __init__(self, op, interface):
+    def __init__(self, op, interface, shifted=True):
         self.op = op
         self.interface = interface
+        self.shifted = shifted
 
         self.dtype = self.op.dtype
         self.shape = self.op.shape
@@ -30,12 +31,19 @@ class JadaPrecOp(object):
     def matvec(self, x):
         alpha = self.op.alpha
         beta = self.op.beta
-        if len(alpha.shape) == 2:
-            alpha = alpha[0, 0]
-            beta = beta[0, 0]
-        elif len(alpha.shape) == 1:
-            alpha = alpha[0]
-            beta = beta[0]
+        try:
+            if len(alpha.shape) == 2:
+                alpha = alpha[0, 0]
+                beta = beta[0, 0]
+            elif len(alpha.shape) == 1:
+                alpha = alpha[0]
+                beta = beta[0]
+        except AttributeError:
+            pass
+
+        if not self.shifted:
+            rhs = x.copy()
+            return self.op.proj(self.interface.solve(self.op.A.fvm_mat, rhs))
 
         rhs = x.copy()
         mat = beta * self.op.A.mat - alpha * self.op.B.mat
@@ -49,22 +57,49 @@ class JadaInterface(NumPyInterface.NumPyInterface):
         self.jac_op = jac_op
         self.mass_op = mass_op
 
-    # def solve(self, op, x, tol):
+    # def solve(self, op, x, tol, maxit):
+    #     if op.dtype.char != op.dtype.char.upper():
+    #         # Real case
+    #         if abs(op.alpha.real) < abs(op.alpha.imag):
+    #             op.alpha = op.alpha.imag
+    #         else:
+    #             op.alpha = op.alpha.real
+    #         op.beta = op.beta.real
+
     #     out = x.copy()
     #     for i in range(x.shape[1]):
-    #         out[:, i] , info = sparse.linalg.gmres(op, x[:, i], restart=100, tol=tol, atol=0,
-    #                                                M=JadaPrecOp(op, self.interface))
-    #         if info != 0:
+    #         out[:, i] , info = sparse.linalg.gmres(op, x[:, i], restart=100, maxiter=10, tol=tol, atol=0,
+    #                                                M=JadaPrecOp(op, self.interface, False))
+    #         if info < 0:
     #             raise Exception('GMRES returned ' + str(info))
+    #         elif info > 0:
+    #             warnings.warn('GMRES did not converge in ' + str(info) + ' iterations')
     #     return out
 
-    def prec(self, x, alpha, beta):
-        if len(alpha.shape) == 2:
-            alpha = alpha[0, 0]
-            beta = beta[0, 0]
-        elif len(alpha.shape) == 1:
-            alpha = alpha[0]
-            beta = beta[0]
+    # def solve(self, op, x, tol, maxit):
+    #     out = x.copy()
+    #     for i in range(x.shape[1]):
+    #         out[:, i] , info = sparse.linalg.gmres(op, x[:, i], restart=100, maxiter=10, tol=tol, atol=0)
+    #         if info < 0:
+    #             raise Exception('GMRES returned ' + str(info))
+    #         elif info > 0:
+    #             warnings.warn('GMRES did not converge in ' + str(info) + ' iterations')
+    #     return out
+
+    def prec(self, x, *args):
+        rhs = x.copy()
+        return self.interface.solve(self.jac_op.fvm_mat, rhs)
+
+    def shifted_prec(self, x, alpha, beta):
+        try:
+            if len(alpha.shape) == 2:
+                alpha = alpha[0, 0]
+                beta = beta[0, 0]
+            elif len(alpha.shape) == 1:
+                alpha = alpha[0]
+                beta = beta[0]
+        except AttributeError:
+            pass
 
         rhs = x.copy()
         mat = beta * self.jac_op.mat - alpha * self.mass_op.mat
--- test/test_jada.py
+++ test/test_jada.py
@@ -1,6 +1,5 @@
 import numpy
 import pytest
-import scipy
 
 from numpy.testing import assert_allclose
 
@@ -14,7 +13,7 @@ import matplotlib.pyplot as plt
 @pytest.fixture(autouse=True, scope='module')
 def import_test():
     try:
-        from fvm import JadaInterface
+        from fvm import JadaInterface # noqa: F401
     except ImportError:
         pytest.skip('jadapy not found')
 
@@ -97,9 +96,8 @@ def test_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
 
     jac_op = JadaInterface.JadaOp(interface.jacobian(x))
     mass_op = JadaInterface.JadaOp(interface.mass_matrix())
-    jada_interface = JadaInterface.JadaInterface(interface, jac_op, mass_op, len(x))
 
-    alpha, beta = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40])
+    alpha, beta = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40], target=0.1)
     jdqz_eigs = numpy.array(sorted(alpha / beta, key=lambda x: abs(x)))
 
     assert_allclose(jdqz_eigs.real, arpack_eigs.real, rtol=0, atol=atol)
@@ -109,7 +107,7 @@ def test_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
         return x
 
     fig, ax = plt.subplots()
-    ax.scatter(eigs.real, eigs.imag, marker='+')
+    ax.scatter(jdqz_eigs.real, jdqz_eigs.imag, marker='+')
     plt.show()
 
 def test_complex_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
@@ -120,7 +118,6 @@ def test_complex_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=F
 
     jac_op = JadaInterface.JadaOp(interface.jacobian(x))
     mass_op = JadaInterface.JadaOp(interface.mass_matrix())
-    jada_interface = JadaInterface.JadaInterface(interface, jac_op, mass_op, len(x), numpy.complex128)
 
     alpha, beta, v = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40],
                                arithmetic='complex', return_eigenvectors=True)
@@ -135,6 +132,82 @@ def test_complex_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=F
         return x
 
     fig, ax = plt.subplots()
-    ax.scatter(eigs.real, eigs.imag, marker='+')
+    ax.scatter(jdqz_eigs.real, jdqz_eigs.imag, marker='+')
     plt.show()
+
+def test_prec_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
+    from fvm import JadaInterface
+    from jadapy import jdqz
+
+    numpy.random.seed(1234)
+
+    jac_op = JadaInterface.JadaOp(interface.jacobian(x))
+    mass_op = JadaInterface.JadaOp(interface.mass_matrix())
+    jada_interface = JadaInterface.JadaInterface(interface, jac_op, mass_op, len(x))
+
+    alpha, beta, v = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40],
+                               return_eigenvectors=True, prec=jada_interface.prec)
+
+    jdqz_eigs = numpy.array(sorted(alpha / beta, key=lambda x: abs(x)))
+
+    assert_allclose(jdqz_eigs.real, arpack_eigs.real, rtol=0, atol=atol)
+    assert_allclose(abs(jdqz_eigs.imag), abs(arpack_eigs.imag), rtol=0, atol=atol)
+
+    if not interactive:
+        return x
+
+    fig, ax = plt.subplots()
+    ax.scatter(jdqz_eigs.real, jdqz_eigs.imag, marker='+')
+    plt.show()
+
+def test_complex_prec_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
+    from fvm import JadaInterface
+    from jadapy import jdqz
+
+    numpy.random.seed(1234)
+
+    jac_op = JadaInterface.JadaOp(interface.jacobian(x))
+    mass_op = JadaInterface.JadaOp(interface.mass_matrix())
+    jada_interface = JadaInterface.JadaInterface(interface, jac_op, mass_op, len(x), numpy.complex128)
+
+    alpha, beta, v = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40],
+                               arithmetic='complex', return_eigenvectors=True, prec=jada_interface.prec)
+    check_eigenvalues(jac_op, mass_op, alpha / beta, v, num_evs, atol)
+
+    jdqz_eigs = numpy.array(sorted(alpha / beta, key=lambda x: abs(x)))
+
+    assert_allclose(jdqz_eigs.real, arpack_eigs.real, rtol=0, atol=atol)
+    assert_allclose(abs(jdqz_eigs.imag), abs(arpack_eigs.imag), rtol=0, atol=atol)
+
+    if not interactive:
+        return x
+
+    fig, ax = plt.subplots()
+    ax.scatter(jdqz_eigs.real, jdqz_eigs.imag, marker='+')
+    plt.show()
+
+def test_complex_shifted_prec_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
+    from fvm import JadaInterface
+    from jadapy import jdqz
+
+    numpy.random.seed(1234)
+
+    jac_op = JadaInterface.JadaOp(interface.jacobian(x))
+    mass_op = JadaInterface.JadaOp(interface.mass_matrix())
+    jada_interface = JadaInterface.JadaInterface(interface, jac_op, mass_op, len(x), numpy.complex128)
+
+    alpha, beta, v = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40],
+                               arithmetic='complex', return_eigenvectors=True, prec=jada_interface.shifted_prec)
+    check_eigenvalues(jac_op, mass_op, alpha / beta, v, num_evs, atol)
+
+    jdqz_eigs = numpy.array(sorted(alpha / beta, key=lambda x: abs(x)))
+
+    assert_allclose(jdqz_eigs.real, arpack_eigs.real, rtol=0, atol=atol)
+    assert_allclose(abs(jdqz_eigs.imag), abs(arpack_eigs.imag), rtol=0, atol=atol)
+
+    if not interactive:
+        return x
+
+    fig, ax = plt.subplots()
+    ax.scatter(jdqz_eigs.real, jdqz_eigs.imag, marker='+')
     plt.show()
