4a0b199638fa00f8fd6b8520a1d3a40183d5f7ed
Sven Baars <s.baars@rug.nl>
2021-03-23 12:33:21 +0100

    JadaInterface: Test preconditioned correction equation solves.

--- test/test_jada.py
+++ test/test_jada.py
@@ -97,7 +97,7 @@ def test_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
     jac_op = JadaInterface.JadaOp(interface.jacobian(x))
     mass_op = JadaInterface.JadaOp(interface.mass_matrix())
 
-    alpha, beta = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40], target=0.1)
+    alpha, beta = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[10, 20], target=0.1)
     jdqz_eigs = numpy.array(sorted(alpha / beta, key=lambda x: abs(x)))
 
     assert_allclose(jdqz_eigs.real, arpack_eigs.real, rtol=0, atol=atol)
@@ -198,6 +198,95 @@ def test_complex_shifted_prec_2D(arpack_eigs, interface, x, num_evs, tol, atol, 
 
     alpha, beta, v = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40],
                                arithmetic='complex', return_eigenvectors=True, prec=jada_interface.shifted_prec)
+    check_eigenvalues(jac_op, mass_op, alpha / beta, v, num_evs, atol)
+
+    jdqz_eigs = numpy.array(sorted(alpha / beta, key=lambda x: abs(x)))
+
+    assert_allclose(jdqz_eigs.real, arpack_eigs.real, rtol=0, atol=atol)
+    assert_allclose(abs(jdqz_eigs.imag), abs(arpack_eigs.imag), rtol=0, atol=atol)
+
+    if not interactive:
+        return x
+
+    fig, ax = plt.subplots()
+    ax.scatter(jdqz_eigs.real, jdqz_eigs.imag, marker='+')
+    plt.show()
+
+def test_complex_solve_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
+    from fvm import JadaInterface
+    from jadapy import jdqz
+
+    numpy.random.seed(1234)
+
+    jac_op = JadaInterface.JadaOp(interface.jacobian(x))
+    mass_op = JadaInterface.JadaOp(interface.mass_matrix())
+    jada_interface = JadaInterface.JadaInterface(interface, jac_op, mass_op, len(x), numpy.complex128)
+
+    assert not jada_interface.preconditioned_solve
+    assert not jada_interface.shifted
+
+    alpha, beta, v = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40],
+                               arithmetic='complex', return_eigenvectors=True, interface=jada_interface)
+    check_eigenvalues(jac_op, mass_op, alpha / beta, v, num_evs, atol)
+
+    jdqz_eigs = numpy.array(sorted(alpha / beta, key=lambda x: abs(x)))
+
+    assert_allclose(jdqz_eigs.real, arpack_eigs.real, rtol=0, atol=atol)
+    assert_allclose(abs(jdqz_eigs.imag), abs(arpack_eigs.imag), rtol=0, atol=atol)
+
+    if not interactive:
+        return x
+
+    fig, ax = plt.subplots()
+    ax.scatter(jdqz_eigs.real, jdqz_eigs.imag, marker='+')
+    plt.show()
+
+def test_complex_prec_solve_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
+    from fvm import JadaInterface
+    from jadapy import jdqz
+
+    numpy.random.seed(1234)
+
+    jac_op = JadaInterface.JadaOp(interface.jacobian(x))
+    mass_op = JadaInterface.JadaOp(interface.mass_matrix())
+    jada_interface = JadaInterface.JadaInterface(interface, jac_op, mass_op, len(x), numpy.complex128,
+                                                 preconditioned_solve=True)
+
+    assert jada_interface.preconditioned_solve
+    assert not jada_interface.shifted
+
+    alpha, beta, v = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40],
+                               arithmetic='complex', return_eigenvectors=True, interface=jada_interface)
+    check_eigenvalues(jac_op, mass_op, alpha / beta, v, num_evs, atol)
+
+    jdqz_eigs = numpy.array(sorted(alpha / beta, key=lambda x: abs(x)))
+
+    assert_allclose(jdqz_eigs.real, arpack_eigs.real, rtol=0, atol=atol)
+    assert_allclose(abs(jdqz_eigs.imag), abs(arpack_eigs.imag), rtol=0, atol=atol)
+
+    if not interactive:
+        return x
+
+    fig, ax = plt.subplots()
+    ax.scatter(jdqz_eigs.real, jdqz_eigs.imag, marker='+')
+    plt.show()
+
+def test_complex_shifted_prec_solve_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
+    from fvm import JadaInterface
+    from jadapy import jdqz
+
+    numpy.random.seed(1234)
+
+    jac_op = JadaInterface.JadaOp(interface.jacobian(x))
+    mass_op = JadaInterface.JadaOp(interface.mass_matrix())
+    jada_interface = JadaInterface.JadaInterface(interface, jac_op, mass_op, len(x), numpy.complex128,
+                                                 preconditioned_solve=True, shifted=True)
+
+    assert jada_interface.preconditioned_solve
+    assert jada_interface.shifted
+
+    alpha, beta, v = jdqz.jdqz(jac_op, mass_op, num_evs, tol=tol, subspace_dimensions=[20, 40],
+                               arithmetic='complex', return_eigenvectors=True, interface=jada_interface)
     check_eigenvalues(jac_op, mass_op, alpha / beta, v, num_evs, atol)
 
     jdqz_eigs = numpy.array(sorted(alpha / beta, key=lambda x: abs(x)))
