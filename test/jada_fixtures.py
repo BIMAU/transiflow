053c438325c49b8facbbdb441aa7b5ac0c7205d8
Sven Baars <s.baars@rug.nl>
2021-03-24 10:53:52 +0100

    test: Move JaDa fixtures to a new file.

---
+++ test/jada_fixtures.py
@@ -0,0 +1,88 @@
+import pytest
+
+import numpy
+
+from numpy.testing import assert_allclose
+
+from scipy import sparse
+
+from fvm import Continuation
+from fvm import Interface
+
+@pytest.fixture(autouse=True, scope='module')
+def import_test():
+    try:
+        from fvm import JadaInterface # noqa: F401
+    except ImportError:
+        pytest.skip('jadapy not found')
+
+@pytest.fixture(scope='module')
+def nx():
+    return 4
+
+@pytest.fixture(scope='module')
+def tol():
+    return 1e-7
+
+@pytest.fixture(scope='module')
+def atol(tol):
+    return tol * 100
+
+@pytest.fixture(scope='module')
+def num_evs():
+    return 9
+
+@pytest.fixture(scope='module')
+def interface(nx):
+    dim = 2
+    dof = 3
+    ny = nx
+    nz = 1
+
+    parameters = {'Reynolds Number': 0}
+    interface = Interface(parameters, nx, ny, nz, dim, dof)
+
+    return interface
+
+@pytest.fixture(scope='module')
+def x(interface):
+    n = interface.dof * interface.nx * interface.ny * interface.nz
+
+    parameters = {}
+    continuation = Continuation(interface, parameters)
+
+    x0 = numpy.zeros(n)
+    x0 = continuation.newton(x0)
+
+    target = 2000
+    ds = 100
+    maxit = 20
+    return continuation.continuation(x0, 'Reynolds Number', target, ds, maxit)
+
+def check_eigenvalues(A_op, B_op, eigs, v, num_evs, tol):
+    from jadapy.utils import norm
+
+    idx = range(len(eigs))
+    idx = numpy.array(sorted(idx, key=lambda i: abs(eigs[i])))
+
+    for i in range(num_evs):
+        j = idx[i]
+        assert norm(A_op @ v[:, j]) > tol
+        assert_allclose(norm(A_op @ v[:, j] - B_op @ v[:, j] * eigs[j]), 0, rtol=0, atol=tol)
+
+@pytest.fixture(scope='module')
+def arpack_eigs(interface, x, num_evs, tol, atol):
+    from fvm import JadaInterface
+
+    A_op = JadaInterface.JadaOp(interface.jacobian(x))
+    B_op = JadaInterface.JadaOp(interface.mass_matrix())
+
+    # A_mat = A_op.mat.todense()
+    # B_mat = B_op.mat.todense()
+
+    # eigs, v = scipy.linalg.eig(A_mat, B_mat, left=False, right=True)
+    eigs, v = sparse.linalg.eigs(A_op, num_evs, B_op, sigma=0.1, tol=tol)
+
+    check_eigenvalues(A_op, B_op, eigs, v, num_evs, atol)
+    eigs = numpy.array(sorted(eigs, key=lambda x: abs(x)))
+    return eigs[:num_evs]
--- test/test_jada.py
+++ test/test_jada.py
@@ -3,12 +3,10 @@ import pytest
 
 from numpy.testing import assert_allclose
 
-from scipy import sparse
-
-from fvm import Continuation
-from fvm import Interface
-
 import matplotlib.pyplot as plt
+
+# Import common fixtures
+from test.jada_fixtures import * # noqa: F401, F403
 
 @pytest.fixture(autouse=True, scope='module')
 def import_test():
@@ -16,49 +14,6 @@ def import_test():
         from fvm import JadaInterface # noqa: F401
     except ImportError:
         pytest.skip('jadapy not found')
-
-@pytest.fixture(scope='module')
-def nx():
-    return 4
-
-@pytest.fixture(scope='module')
-def tol():
-    return 1e-7
-
-@pytest.fixture(scope='module')
-def atol(tol):
-    return tol * 100
-
-@pytest.fixture(scope='module')
-def num_evs():
-    return 9
-
-@pytest.fixture(scope='module')
-def interface(nx):
-    dim = 2
-    dof = 3
-    ny = nx
-    nz = 1
-
-    parameters = {'Reynolds Number': 0}
-    interface = Interface(parameters, nx, ny, nz, dim, dof)
-
-    return interface
-
-@pytest.fixture(scope='module')
-def x(interface):
-    n = interface.dof * interface.nx * interface.ny * interface.nz
-
-    parameters = {}
-    continuation = Continuation(interface, parameters)
-
-    x0 = numpy.zeros(n)
-    x0 = continuation.newton(x0)
-
-    target = 2000
-    ds = 100
-    maxit = 20
-    return continuation.continuation(x0, 'Reynolds Number', target, ds, maxit)
 
 def check_eigenvalues(A_op, B_op, eigs, v, num_evs, tol):
     from jadapy.utils import norm
@@ -70,23 +25,6 @@ def check_eigenvalues(A_op, B_op, eigs, v, num_evs, tol):
         j = idx[i]
         assert norm(A_op @ v[:, j]) > tol
         assert_allclose(norm(A_op @ v[:, j] - B_op @ v[:, j] * eigs[j]), 0, rtol=0, atol=tol)
-
-@pytest.fixture(scope='module')
-def arpack_eigs(interface, x, num_evs, tol, atol):
-    from fvm import JadaInterface
-
-    A_op = JadaInterface.JadaOp(interface.jacobian(x))
-    B_op = JadaInterface.JadaOp(interface.mass_matrix())
-
-    # A_mat = A_op.mat.todense()
-    # B_mat = B_op.mat.todense()
-
-    # eigs, v = scipy.linalg.eig(A_mat, B_mat, left=False, right=True)
-    eigs, v = sparse.linalg.eigs(A_op, num_evs, B_op, sigma=0.1, tol=tol)
-
-    check_eigenvalues(A_op, B_op, eigs, v, num_evs, atol)
-    eigs = numpy.array(sorted(eigs, key=lambda x: abs(x)))
-    return eigs[:num_evs]
 
 def test_2D(arpack_eigs, interface, x, num_evs, tol, atol, interactive=False):
     from fvm import JadaInterface
