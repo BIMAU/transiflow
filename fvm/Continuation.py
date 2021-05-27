94e2c3a4b9736b848fc690a66b7ed51c2c7d3982
ipelupessy <i.pelupessy@esciencecenter.nl>
2021-05-27 12:12:11 +0200

    Continaution: fix reporting (remove abs)

--- fvm/Continuation.py
+++ fvm/Continuation.py
@@ -150,7 +150,7 @@ class Continuation:
         ds = numpy.sign(ds) * min(max(abs(ds), min_step_size), max_step_size)
 
         if self.parameters.get('Verbose', False):
-            print('New stepsize: |ds|=%e, factor=%e' % (ds, factor))
+            print('New stepsize: ds=%e, factor=%e' % (ds, factor))
             sys.stdout.flush()
 
         return ds
