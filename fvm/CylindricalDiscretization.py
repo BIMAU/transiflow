87e3670ee38df8c3154a5fc81e5f6c2b5a9cc98c
Sven Baars <s.baars@rug.nl>
2021-10-05 12:34:37 +0200

    CylindricalDiscretization: Fix a minus sign.

--- fvm/CylindricalDiscretization.py
+++ fvm/CylindricalDiscretization.py
@@ -46,7 +46,7 @@ class CylindricalDiscretization(Discretization):
             Re = 1
 
         return 1 / Re * (self.iruscale(self.u_rr()) + self.iru2scale(self.u_tt() - self.value_u() - 2 * self.v_t_u())
-                         + self.irvscale(self.v_rr()) + self.irv2scale(self.v_tt() - self.value_v() - 2 * self.u_t_v())) \
+                         + self.irvscale(self.v_rr()) + self.irv2scale(self.v_tt() - self.value_v() + 2 * self.u_t_v())) \
             - (self.p_r() + self.irvscale(self.p_t())) \
             + self.div()
 
@@ -62,7 +62,7 @@ class CylindricalDiscretization(Discretization):
 
         return 1 / Re * (self.iruscale(self.u_rr()) + self.iru2scale(self.u_tt() - self.value_u() - 2 * self.v_t_u())
                          + self.u_zz()
-                         + self.irvscale(self.v_rr()) + self.irv2scale(self.v_tt() - self.value_v() - 2 * self.u_t_v())
+                         + self.irvscale(self.v_rr()) + self.irv2scale(self.v_tt() - self.value_v() + 2 * self.u_t_v())
                          + self.v_zz()
                          + self.irvscale(self.w_rr()) + self.irv2scale(self.w_tt()) + self.w_zz()) \
             - (self.p_r() + self.irvscale(self.p_t()) + self.p_z()) \
