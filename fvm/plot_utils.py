c560c57ff90fd3e652fe82d9dec934b604bab5cc
Sven Baars <s.baars@rug.nl>
2021-11-30 14:26:17 +0100

    plot_utils: Factor out some plotting functionality and make everything optional.

--- fvm/plot_utils.py
+++ fvm/plot_utils.py
@@ -4,51 +4,45 @@ import matplotlib.pyplot as plt
 from fvm import utils
 from fvm.utils import create_state_mtx # noqa: F401
 
-def plot_velocity_magnitude(state, interface):
-    m = utils.compute_velocity_magnitude(state, interface)
-
-    x = interface.discretization.x[:-3]
-    y = interface.discretization.y[:-3]
-    x, y = numpy.meshgrid(x, y)
-
-    fig1, ax1 = plt.subplots()
-    cs = ax1.contourf(x, y, m.transpose(), 15)
-    fig1.colorbar(cs)
-
-    ax1.vlines(x[0, :], *y[[0, -1], 0], colors='0.3', linewidths=0.5)
-    ax1.hlines(y[:, 0], *x[0, [0, -1]], colors='0.3', linewidths=0.5)
-
-    plt.show()
-
-def plot_streamfunction(state, interface):
-    psi = utils.compute_streamfunction(state, interface)
-
-    x = interface.discretization.x[:-3]
-    y = interface.discretization.y[:-3]
-    x, y = numpy.meshgrid(x, y)
-
-    fig1, ax1 = plt.subplots()
-    cs = ax1.contourf(x, y, psi.transpose(), 15)
-    fig1.colorbar(cs)
-
-    ax1.vlines(x[0, :], *y[[0, -1], 0], colors='0.3', linewidths=0.5)
-    ax1.hlines(y[:, 0], *x[0, [0, -1]], colors='0.3', linewidths=0.5)
-
-    plt.show()
-
-def plot_value(t, interface=None, x=None, y=None):
+def get_meshgrid(interface, x=None, y=None):
     if x is None:
         x = interface.discretization.x[:-3]
     if y is None:
         y = interface.discretization.y[:-3]
 
-    x, y = numpy.meshgrid(x, y)
+    return numpy.meshgrid(x, y)
 
-    fig1, ax1 = plt.subplots()
-    cs = ax1.contourf(x, y, t.transpose(), 15)
-    fig1.colorbar(cs)
+def plot_contour(x, y, value, legend=True, grid=True, show=True):
+    fig, ax = plt.subplots()
+    cs = ax.contourf(x, y, value.transpose(), 15)
 
-    ax1.vlines(x[0, :], *y[[0, -1], 0], colors='0.3', linewidths=0.5)
-    ax1.hlines(y[:, 0], *x[0, [0, -1]], colors='0.3', linewidths=0.5)
+    if legend:
+        fig.colorbar(cs)
 
-    plt.show()
+    if grid:
+        ax.vlines(x[0, :], *y[[0, -1], 0], colors='0.3', linewidths=0.5)
+        ax.hlines(y[:, 0], *x[0, [0, -1]], colors='0.3', linewidths=0.5)
+
+    if show:
+        plt.show()
+
+    return fig
+
+def plot_velocity_magnitude(state, interface, *args, **kwargs):
+    m = utils.compute_velocity_magnitude(state, interface)
+
+    x, y = get_meshgrid(interface)
+
+    return plot_contour(x, y, m, *args, **kwargs)
+
+def plot_streamfunction(state, interface, *args, **kwargs):
+    psi = utils.compute_streamfunction(state, interface)
+
+    x, y = get_meshgrid(interface)
+
+    return plot_contour(x, y, psi, *args, **kwargs)
+
+def plot_value(t, interface=None, x=None, y=None, *args, **kwargs):
+    x, y = get_meshgrid(interface, x, y)
+
+    return plot_contour(x, y, t, *args, **kwargs)
