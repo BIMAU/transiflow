cb7cf900baa42ffd00372d45bb13539e6f07ad76
Jonas Thies <16190001+jthies@users.noreply.github.com>
2022-08-22 22:24:48 +0200

    add plotting functionality and fix some indexing bugs in StokesDD class

--- solver/solver_util.py
+++ solver/solver_util.py
@@ -22,8 +22,8 @@ def get_z_ordering(nx, ny, dof=1, x_offset=0, y_offset=0):
      /  /  /  /
     *--*  *--*
 
-    Returns z_idx and z_inv such that if v is in cartesian ordering,
-    w=v[z_idx] is in z-ordering, and v=w[z_inv].
+    Returns z_idx such that if v is in cartesian ordering,
+    w=v[z_idx] is in z-ordering.
     To reorder a sparse (scipy)  matrix, you can use A[z_idx,:][:,z_idx].
 
     This function is currently restricted to 2D but allows
@@ -91,6 +91,13 @@ def get_inv_ordering(idx):
     inv[idx]=range(idx.size)
     return inv
 
+def flatten(list_of_lists):
+    '''
+    Given something like [[1,2],[3],[4,5,6]],
+    returns [1,2,3,4,5,6]
+    '''
+    return [item for sublist in list_of_lists for item in sublist]
+
 def get_subdomain_groups(dims, sd_dims, dof):
     '''
     Returns an orthonormal sparse matrix V with, for block size=prod(sd_dims),
@@ -143,8 +150,8 @@ def get_subdomain_groups(dims, sd_dims, dof):
         return range(sd*block_size*dof+var, (sd+1)*block_size*dof+var, dof)
 
     valV  = numpy.ones(N) / sqrt(block_size)
-    rows = numpy.array(range(N))
-    cols = numpy.zeros(N)
+    rows = numpy.array(range(N),dtype='int')
+    cols = numpy.zeros(N,dtype='int')
     for i in range(k):
         var = i % dof
         sd  = int(( i - var)/dof)
@@ -153,53 +160,215 @@ def get_subdomain_groups(dims, sd_dims, dof):
     V = scipy.sparse.csc_matrix( (valV, (rows, cols)), shape=[N,k])
     return V
 
-def get_separator_groups(nx,sx,ny,sy,nz=1,sz=1,dof=1):
+class StokesDD:
     '''
-    Returns a sparse matrix in CSC format with N=nx*ny*nz*dof rows
-    and m<<N columns. Each column index represents a 'separator group',
-    marked in the following sketch by its column index. Note that interior
-    variables are all grouped together in group 0.
-    For nx=ny=8, sx=sy=3:
+    Assuming a 2D Stokes equation on an nx x ny C-grid,
+    and a subdomain size of sx*sy cells per subdomain,
+    and z-curve ordering, this function returns i1, i2 and i3
+    such that
+    A(i1,i1) represents interior variables of the subdomain,
+    and the i2 variables are the velocity separators. i3 are
+    the pressure indices, i3 is a subset of i1.
 
-    0  0  1  0  0  4  0  0
-    0  0  1  0  0  4  0  0
-    2  2  3  5  5  6 14 14
-    0  0  7  0  0 10  0  0
-    0  0  7  0  0 10  0  0
-    8  8  9 11 11 12 16 16
-    0  0 17  0  0 20  0  0
-    0  0 17  0  0 20  0  0
+    The intended use of these arrays is that
 
-    These indices are obtained using a z-curve for the subdomains,
-    so there may be 'holes' in the indexing, e.g. at the right boundary
-    where there are no separators. We therefore renumber them after construction
-    to give coniguous column indices.
-
+    V2  with V2[i2,sd]=v2 is a partition of unity of the velocity separators of the domain,
+    and V1 = A11\(A12 V2) is the 'harmonic extension' to the interior variables.
+    V3 with
     '''
 
-    dim = 2
-    if nz != 1:
-        dim = 3
-        raise Exception('not implemented for 3D yet')
+    def __init__(self, nx, ny, sx, sy):
+        '''
+        '''
+        self.dim=2
+        self.dof=3
+        self.nx=nx
+        self.ny=ny
+        self.sx=sx
+        self.sy=sy
 
-    if dof > dim:
-        raise Exception('not implemented for Stokes-like problems')
+        self.z_idx = get_z_ordering(nx,ny,dof=1)
+        self.z_inv = get_inv_ordering(self.z_idx)
+        self.Z = self.z_inv.reshape(nx,ny)
+
+        self.num_sd_x = ceil(nx/sx)
+        self.num_sd_y = ceil(ny/sy)
+
+        # create a z-ordering for the subdomains
+        self.sd_idx = get_z_ordering(self.num_sd_x, self.num_sd_y, dof=1)
+        self.sd_inv = get_inv_ordering(self.sd_idx)
+        self.Zsd = self.sd_inv.reshape(self.num_sd_x,self.num_sd_y)
+
+    def plot_cgrid(self, indices, colors, title=None, markersize=12, ax=None, plotgrid=True):
+        '''
+        given a set of indices into the c-grid
+        (e.g. as returned by the indices() function),
+        make a plot of the C-grid with these variables
+        marked. Both indices and colors should be lists
+        (indices a list of lists) of equal length.
+        '''
+        from matplotlib import pyplot as plt
+
+        if len(colors) != len(indices):
+            raise Exception('Usage: plot_cgrid([i1,i2,...],[c1,c2,...],title) with same length of the list of index lists and list of colors.')
+
+        if ax is None:
+            plotgrid=True
+            ax=plt.axes()
+
+        if plotgrid:
+            for j in range(self.ny+1):
+                ax.plot([-0.5,self.nx-0.5],[-j+0.5,-j+0.5],'-')
+            for i in range(self.nx+1):
+                ax.plot([i-0.5,i-0.5],[0.5-self.ny,0.5],'-')
+
+        if title is not None:
+            plt.title(title)
+
+        MarkerDict = dict(([0,'>'],[1,'v'],[2,'o']))
+        ioff = [0.5, 0,   0]
+        joff = [0,   -0.5, 0]
+
+        for set in range(len(indices)):
+            idx = indices[set]
+            clr = colors[set]
+            for z_id in idx:
+                cell  = int(z_id/self.dof)
+                var   = z_id % self.dof
+                i, j = self.cell_coordinates(cell)
+                ax.plot([i+ioff[var]],[-j+joff[var]],MarkerDict[var], markersize=12, markerfacecolor=clr)
+        plt.draw()
 
 
-    # we use z-ordering of the subdomain IDs, which will produce holes in the
-    # numbering of subdomains unless nx is a multiple of sx, etc. This can be
-    # fixed in a future 'proper' implementation.
-    if nx%sx or ny%sy:
-        raise Exception('up to now, nx must be a multiple of sx, and similar for ny resp. sy.')
 
-    num_sd_x = ceil(nx/sx)
-    num_sd_y = ceil(ny/sy)
+    def subdomain_coordinates(self, sd):
+        '''
+        Given a subdomain ID (in z-ordering),
+        returns indices i and j of that subdomain
+        in a Cartesian grid of subdomains.
+        '''
+        # note that this can be implemented 'on-the-fly' using
+        # proper space-filling curve algorithms, but for now because
+        # we don't want discontiguous indices, we do it using thsi look-up:
+        ic,jc = numpy.where(self.Zsd==sd)
+        return ic[0], jc[0]
 
-    sd_z_idx, sd_z_inv = get_z_ordering(num_sd_x, num_sd_y, dof=3*dof)
+    def cell_coordinates(self, cell):
+        '''
+        Given a grid cell ID (in z-ordering),
+        returns indices i and j of that grid cell
+        in a Cartesian grid of nx x ny cells.
+        '''
+        # note that this can be implemented 'on-the-fly' using
+        # proper space-filling curve algorithms, but for now because
+        # we don't want discontiguous indices, we do it using thsi look-up:
+        ic,jc = numpy.where(self.Z==cell)
+        return ic[0], jc[0]
 
-    print(sd_z_idx)
-    print(sd_z_inv)
+    def indices(self, sd):
+        '''
+        For subdomain 'sd' (0<=sd<=self.num_subdomains),
+        returns idx0, idx1, idx2  and idx3 such that, if A is the Jacobian of a 'Stokes-like' problem
+        on a C-grid, 
 
+        A[idx0,:][:,idx0] is the subdomain matrix with minimal overlap, i.e. for this 2x2 domain,
+        the marked variables (o: p, T; >: u; '^': v)
+
+            +-^-+-^-+
+            |   |   |
+            > o > o >
+            |   |   |
+            +-^-+-^-+
+            |   |   |
+            > o > o >
+            |   |   |
+            +-^-+-^-+
+
+        A[idx1,:][:,idx1] is the interior (*: excluded variables)
+
+            +-*-+-*-+
+            |   |   |
+            * o > o *
+            |   |   |
+            +-^-+-*-+
+            |   |   |
+            * o * o *
+            |   |   |
+            +-*-+-*-+
+
+            A[idx2,:][:,idx2] are the separators,
+
+
+        of the subdomain, and idx2 are the indices of the separator velocities.
+        idx3 are the pressure nodes of the subdomain (a subset of idx1).
+
+            +-2-+-2-+
+            |   |   |
+            2 3 > 3 2
+            |   |   |
+            +-^-+-2-+
+            |   |   |
+            2 3 2 3 2
+            |   |   |
+            +-2-+-2-+
+
+        To make further processing simpler, the idx2 variables (velocity separators)
+        are returned as a 'list of lists', grouped by variable type and subdomain face.
+
+        '''
+        # get the coordinates of the first cell in the subdomain
+        # (this is a robust but slow implementation, we need it for now
+        # because our z-ordering constructs an array rather than just
+        # providing a function, as a 'real' implementation would)
+        ic, jc = self.subdomain_coordinates(sd)
+
+        irng = range(ic*self.sx, min((ic+1)*self.sx, self.nx))
+        jrng = range(jc*self.sx, min((jc+1)*self.sy, self.ny))
+        #i-index of grid cells to the left of the subdomain
+        im1 = []
+        if ic > 0:
+            im1 += [irng[0]-1]
+        # j-index of the grids cell on top of the current subdomain
+        jm1 = []
+        if jc > 0:
+            jm1 += [jrng[0]-1]
+
+        # all subdomain variables (including minimal overlap to neighboring subdomains):
+        idx0  = []
+        idx0 += list((0+self.dof*self.Z[im1+list(irng),:][:,jrng]).flat)
+        idx0 += list((1+self.dof*self.Z[im1+list(irng),:][:,jrng]).flat)
+        idx0 += list((2+self.dof*self.Z[irng,:][:,list(jrng)]).flat)
+
+        # interior nodes: u-velocities
+        idx1 = list((0+self.dof*self.Z[range(irng[0],irng[-1]),:][:,range(jrng[0],jrng[-1])]).flat)
+        # interior nodes: v-velocities
+        idx1 += list((1+self.dof*self.Z[range(irng[0],irng[-1]),:][:,range(jrng[0],jrng[-1])]).flat)
+        # pressure
+        idx3 = list((2+self.dof*self.Z[irng,:][:,jrng]).flat)
+        idx1 += idx3
+
+        # last row and column of u's / v's are either separator velocities or interior
+        idx2 = []
+        last_u1 = list((0+self.dof*self.Z[irng[-1],:][jrng]).flat)
+        last_v1 = list((1+self.dof*self.Z[irng[-1],:][range(jrng[0],jrng[-1])]).flat)
+        last_u2 = list((0+self.dof*self.Z[range(irng[0],irng[-1]),:][:,jrng[-1]]).flat)
+        last_v2 = list((1+self.dof*self.Z[irng,:][:,jrng[-1]]).flat)
+
+        if ic < self.num_sd_x-1:
+            idx2.append(last_u1)
+            idx2.append(last_v1)
+        else:
+            idx1.append(last_u1[:])
+            idx1.append(last_v1[:])
+
+        if jc < self.num_sd_y-1:
+            idx2.append(last_u2)
+            idx2.append(last_v2)
+        else:
+            idx1.append(last_u2[:])
+            idx1.append(last_v2[:])
+
+        return sorted(idx0), idx1, idx2, idx3
 
 class BlockJacobi(spla.LinearOperator):
 
--- solver/test_separators.py
+++ solver/test_separators.py
@@ -1,13 +1,42 @@
+from matplotlib import pyplot as plt
 from solver_util import *
 
-nx=16
-ny=16
+nx=8
+ny=6
 
 # note: this currently only works if
 # nx is a multiple of sx, etc., because otherwise
 # we'll get holes and we need to reindex the z-curve.
 sx=4
 sy=4
-dof=1
+dof=3
 
-V = get_separator_groups(nx,sx,ny,sy,dof=dof)
+DD = StokesDD(nx,ny,sx,sy)
+
+sd=0
+i0, i1, i2, i3 =DD.indices(sd)
+
+fig, axs = plt.subplots(2)
+DD.plot_cgrid([i0],['yellow'],title='overlapping subdomain',markersize=12, ax=axs[0], plotgrid=True)
+DD.plot_cgrid([i1,flatten(i2),i3],['white','red','yellow'],title='groups',ax=axs[1], plotgrid=True)
+
+sd=1
+i0, i1, i2, i3 =DD.indices(sd)
+DD.plot_cgrid([i0],['blue'],markersize=10, ax=axs[0], plotgrid=False)
+
+#DD.plot_cgrid(nx,ny,i1,'interior of subdomain')
+#pDD.lot_cgrid(nx,ny,i2,'separators of subdomain')
+#DD.plot_cgrid(nx,ny,i2,'pressures of subdomain')
+
+#V2 = get_separator_groups(nx,sx,ny,sy,dof=dof)
+#numpy.set_printoptions(precision=3, linewidth=128, threshold=1000,edgeitems=200)
+
+
+#print('nx=%d, sx=%d'%(nx,sx))
+#print('V2.shape='+str(V2.shape))
+#print('V2:')
+#print(V2.todense())
+
+axs[0].set_aspect('equal', adjustable='box')
+axs[1].set_aspect('equal', adjustable='box')
+plt.show()
