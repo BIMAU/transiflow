44f7672001a4532077a77764a31f8dcb9da863a7
dajuno <dajuno@riseup.net>
2023-02-14 13:21:52 +0100

    Initial PETSc interface

---
+++ fvm/PETScInterface.py
@@ -0,0 +1,141 @@
+import os
+import sys
+
+import numpy
+from petsc4py import PETSc
+
+import fvm
+
+
+class Vector(PETSc.Vec):
+    """Distributed Epetra_Vector with some extra methods added to it for convenience."""
+
+    @staticmethod
+    def from_array(x):
+        vec = PETSc.Vec().createSeq(len(x))
+        vec.setArray(x)
+        return vec
+
+    size = property(PETSc.Vec.getSize)
+
+
+def ind2sub(nx, ny, nz, idx, dof=1):
+    rem = idx
+    var = rem % dof
+    rem = rem // dof
+    i = rem % nx
+    rem = rem // nx
+    j = rem % ny
+    rem = rem // ny
+    k = rem % nz
+    return (i, j, k, var)
+
+
+def sub2ind(nx, ny, nz, dof, i, j, k, var):
+    return ((k * ny + j) * nx + i) * dof + var
+
+
+def set_default_parameter(parameterlist, name, value):
+    if name not in parameterlist:
+        parameterlist[name] = value
+
+    return parameterlist[name]
+
+
+def get_local_coordinate_vector(x, nx_offset, nx_local):
+    x = numpy.roll(x, 2)
+    x = x[nx_offset : nx_offset + nx_local + 3]
+    return numpy.roll(x, -2)
+
+
+class Interface(fvm.Interface):
+    """This class defines an interface to the PETSc backend for the
+    discretization. We use this so we can write higher level methods
+    such as pseudo-arclength continuation without knowing anything
+    about the underlying methods such as the solvers that are present
+    in the backend we are interfacing with.
+
+    The PETSc backend partitions the domain into Cartesian subdomains,
+    while solving linear systems on skew Cartesian subdomains to deal
+    with the C-grid discretization. The subdomains will be distributed
+    over multiple processors if MPI is used to run the application."""
+
+    def __init__(self, comm, parameters, nx, ny, nz, dim, dof):
+        fvm.Interface.__init__(self, parameters, nx, ny, nz, dim, dof)
+
+        self.jac = None
+
+        self.nx_global = nx
+        self.ny_global = ny
+        self.nz_global = nz
+        self.dof = dof
+
+        self.comm = comm
+
+        self._original_stdout = sys.stdout
+        if self.comm.rank != 0:
+            self.debug_print("PID %d: Disable output to stdout" % self.comm.rank)
+            sys.stdout = open(os.devnull, "w")
+
+        self.parameters = parameters
+
+    def __del__(self):
+        if self.comm.rank != 0:
+            sys.stdout.close()
+            sys.stdout = self._original_stdout
+            self.debug_print("PID %d: Re-enable output to stdout" % self.comm.rank)
+
+    def rhs(self, state):
+        """Right-hand side in M * du / dt = F(u) defined on the
+        non-overlapping discretization domain map."""
+
+        rhs = fvm.Interface.rhs(self, state.getArray())
+        rhs = Vector.from_array(rhs)
+
+        return rhs
+
+    def jacobian(self, state):
+        """Jacobian J of F in M * du / dt = F(u) defined on the
+        domain map used by PETSc."""
+
+        local_jac = fvm.Interface.jacobian(self, state.getArray())
+
+        if self.jac is None:
+            self.jac = PETSc.Mat().createAIJ((state.size, state.size), comm=self.comm)
+            self.jac.setUp()
+        else:
+            self.jac.zerosEntries()
+
+        self.jac.setValuesCSR(
+            numpy.array(local_jac.begA, dtype=numpy.int32),
+            numpy.array(local_jac.jcoA[: local_jac.begA[-1]], dtype=numpy.int32),
+            local_jac.coA[: local_jac.begA[-1]],
+        )
+
+        self.jac.assemble()
+
+        return self.jac
+
+    def mass_matrix(self):
+        """Mass matrix M in M * du / dt = F(u) defined on the
+        domain map used by HYMLS."""
+        return None
+
+    def solve(self, jac, rhs):
+        """Currently unused direct solver that was used for testing."""
+
+        ksp = PETSc.KSP().create()
+        ksp.setType("preonly")
+        pc = ksp.getPC()
+        pc.setType("lu")
+        ksp.setFromOptions()
+
+        ksp.setOperators(jac)
+        x = Vector(rhs)
+
+        ksp.solve(rhs, x)
+        return x
+
+    def eigs(self, state, return_eigenvectors=False, enable_recycling=False):
+        """Compute the generalized eigenvalues of beta * J(x) * v = alpha * M * v."""
+        raise NotImplementedError()
---
+++ tests/test_PETSc.py
@@ -0,0 +1,105 @@
+import os
+
+import numpy
+import pytest
+
+
+def read_matrix(fname):
+    from petsc4py import PETSc
+
+    dirname = os.path.dirname(__file__)
+    rows = []
+    cols = []
+    vals = []
+    with open(os.path.join(dirname, fname), "r") as f:
+        for i in f.readlines():
+            r, c, v = [j.strip() for j in i.strip().split(" ") if j]
+            r = int(r) - 1
+            c = int(c) - 1
+            v = float(v)
+            rows.append(r)
+            cols.append(c)
+            vals.append(v)
+
+    size = max(rows) + 1
+    A = PETSc.Mat().createAIJ((size, size))
+    A.setUp()
+
+    for r, c, v in zip(rows, cols, vals):
+        A.setValue(r, c, v)
+    A.assemble()
+
+    return A
+
+
+def read_vector(fname):
+    from fvm import PETScInterface
+
+    values = []
+    dirname = os.path.dirname(__file__)
+    with open(os.path.join(dirname, fname), "r") as f:
+        for i, v in enumerate(f.readlines()):
+            values.append(float(v.strip()))
+
+    vec = PETScInterface.Vector.from_array(values)
+
+    return vec
+
+
+def extract_sorted_row(A, i):
+    indices, values = A.getRow(i)
+    idx = sorted(range(len(indices)), key=lambda i: indices[i])
+    return [indices[i] for i in idx], [values[i] for i in idx]
+
+
+def test_ldc():
+    try:
+        from mpi4py import MPI
+
+        from fvm import PETScInterface
+    except ImportError:
+        pytest.skip("PETSc not found")
+
+    nx = 4
+    ny = nx
+    nz = nx
+    dim = 3
+    dof = 4
+    parameters = {"Reynolds Number": 100}
+    n = nx * ny * nz * dof
+
+    state = numpy.zeros(n)
+    for i in range(n):
+        state[i] = i + 1
+
+    comm = MPI.COMM_WORLD
+    interface = PETScInterface.Interface(comm, parameters, nx, ny, nz, dim, dof)
+
+    state = PETScInterface.Vector.from_array(state)
+
+    A = interface.jacobian(state)
+    rhs = interface.rhs(state)
+
+    B = read_matrix("ldc_%sx%sx%s.txt" % (nx, ny, nz))
+    rhs_B = read_vector("ldc_rhs_%sx%sx%s.txt" % (nx, ny, nz))
+
+    for i in range(n):
+        indices_A, values_A = extract_sorted_row(A, i)
+        indices_B, values_B = extract_sorted_row(B, i)
+
+        print("Expected:")
+        print(indices_B)
+        print(values_B)
+
+        print("Got:")
+        print(indices_A)
+        print(values_A)
+
+        assert len(indices_A) == len(indices_B)
+        for j in range(len(indices_A)):
+            assert indices_A[j] == indices_B[j]
+            assert values_A[j] == pytest.approx(values_B[j])
+
+    for i in range(n):
+        print(i, rhs[i], rhs_B[i])
+        assert rhs_B[i] == pytest.approx(rhs[i])
