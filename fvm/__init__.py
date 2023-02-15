dd501ec79c889c5a55f9537d3038cd131e2c9f11
Sven Baars <s.baars@uu.nl>
2023-02-15 13:11:34 +0100

    Export some modules for backward compatibility.

--- fvm/__init__.py
+++ fvm/__init__.py
@@ -5,5 +5,16 @@ from .CylindricalDiscretization import CylindricalDiscretization
 from .Continuation import Continuation
 from .TimeIntegration import TimeIntegration
 
-__all__ = ['CrsMatrix', 'BoundaryConditions', 'Discretization', 'CylindricalDiscretization',
-           'Continuation', 'TimeIntegration']
+# Backward compatibility
+from .interface.SciPy import Interface
+from .interface import SciPy as SciPyInterface
+
+from .interface import Epetra as EpetraInterface
+from .interface import HYMLS as HYMLSInterface
+from .interface import PETSc as PETScInterface
+
+__all__ = [
+    'CrsMatrix', 'BoundaryConditions', 'Discretization', 'CylindricalDiscretization',
+    'Continuation', 'TimeIntegration', 'Interface', 'SciPyInterface', 'EpetraInterface',
+    'HYMLSInterface', 'PETScInterface'
+]
