51241460070ac334c49c64a40b878266785764bf
Sven Baars <s.baars@uu.nl>
2023-02-15 13:47:15 +0100

    init: Use try to import optional modules.

--- fvm/__init__.py
+++ fvm/__init__.py
@@ -5,16 +5,31 @@ from .CylindricalDiscretization import CylindricalDiscretization
 from .Continuation import Continuation
 from .TimeIntegration import TimeIntegration
 
-# Backward compatibility
-from .interface.SciPy import Interface
-from .interface import SciPy as SciPyInterface
-
-from .interface import Epetra as EpetraInterface
-from .interface import HYMLS as HYMLSInterface
-from .interface import PETSc as PETScInterface
-
 __all__ = [
     'CrsMatrix', 'BoundaryConditions', 'Discretization', 'CylindricalDiscretization',
-    'Continuation', 'TimeIntegration', 'Interface', 'SciPyInterface', 'EpetraInterface',
-    'HYMLSInterface', 'PETScInterface'
+    'Continuation', 'TimeIntegration'
 ]
+
+# Backward compatibility
+from .interface.SciPy import Interface # noqa: F401
+from .interface import SciPy as SciPyInterface # noqa: F401
+
+__all__.extend(['Interface', 'SciPyInterface'])
+
+try:
+    from .interface import Epetra as EpetraInterface # noqa: F401
+    __all__.extend(['EpetraInterface'])
+except ImportError:
+    pass
+
+try:
+    from .interface import HYMLS as HYMLSInterface # noqa: F401
+    __all__.extend(['HYMLSInterface'])
+except ImportError:
+    pass
+
+try:
+    from .interface import PETSc as PETScInterface # noqa: F401
+    __all__.extend(['PETScInterface'])
+except ImportError:
+    pass
