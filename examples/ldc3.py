98e812a328c7636f920e372ef4a2af36c340a537
Sven Baars <s.baars@uu.nl>
2023-03-02 13:07:20 +0100

    interface: Export the create() method as Interface constructor.

--- examples/ldc3.py
+++ examples/ldc3.py
@@ -2,10 +2,9 @@ import numpy
 
 import matplotlib.pyplot as plt
 
+from fvm import Interface
 from fvm import TimeIntegration
 from fvm import utils
-
-from fvm.interface.SciPy import Interface
 
 
 class Data:
