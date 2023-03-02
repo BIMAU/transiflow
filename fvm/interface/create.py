d9b8ddb984aeb317807ad7121d01ac53ae24a921
Sven Baars <s.baars@uu.nl>
2023-03-02 12:54:50 +0100

    interface: Add a create method.

---
+++ fvm/interface/create.py
@@ -0,0 +1,18 @@
+def _icmp(first, second):
+    return first.lower() == second.lower()
+
+def create(parameters, nx, ny, nz, dim, dof, backend='SciPy'):
+    if _icmp(backend, 'Epetra'):
+        from .Epetra import Interface
+        return Interface(parameters, nx, ny, nz, dim, dof)
+
+    if _icmp(backend, 'HYMLS'):
+        from .HYMLS import Interface
+        return Interface(parameters, nx, ny, nz, dim, dof)
+
+    if _icmp(backend, 'PETSc'):
+        from .PETSc import Interface
+        return Interface(parameters, nx, ny, nz, dim, dof)
+
+    from .SciPy import Interface
+    return Interface(parameters, nx, ny, nz, dim, dof)
