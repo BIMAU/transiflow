eee8794d3db6b770687139d912a3fe84c68aa6c3
Sven Baars <s.baars@uu.nl>
2023-03-05 14:28:39 +0100

    tests: Add a test for to_dense().

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -8,6 +8,14 @@ def get_test_matrix():
     begA = numpy.array([0, 2, 3, 5], dtype=int)
     return CrsMatrix(coA, jcoA, begA)
 
+def get_random_test_matrix():
+    m = 5
+    n = 3
+    coA = numpy.array(numpy.random.random(m * n), dtype=int)
+    jcoA = numpy.repeat(numpy.array([numpy.arange(n)]), m)
+    begA = numpy.arange(m * n + 1, step=n)
+    return CrsMatrix(coA, jcoA, begA, m=m, n=n)
+
 def test_getitem():
     A = get_test_matrix()
     assert A[0, 1] == 0
@@ -31,3 +39,16 @@ def test_getitem_array():
     assert B[0, 1] == A[0, 2]
     assert B[1, 0] == A[1, 0]
     assert B[1, 1] == A[1, 2]
+
+def test_to_dense():
+    A = get_test_matrix()
+    B = A.to_dense()
+    for i in range(A.m):
+        for j in range(A.n):
+            assert A[i, j] == B[i, j]
+
+    A = get_random_test_matrix()
+    B = A.coA.copy().reshape(A.m, A.n)
+    for i in range(A.m):
+        for j in range(A.n):
+            assert A[i, j] == B[i, j]
