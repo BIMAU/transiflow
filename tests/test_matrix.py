a3cddc2cecf7a99a1917528cb2fc4649c387c35b
Sven Baars <s.baars@uu.nl>
2023-03-05 15:11:44 +0100

    CrsMatrix: Implement __setitem__().

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -19,13 +19,14 @@ def get_random_test_matrix():
 
 def test_getitem():
     A = get_test_matrix()
+
     assert A[0, 1] == 0
     assert A[1, 1] == 3
 
 def test_getitem_range():
     A = get_test_matrix()
     B = A[0:2, 1:3]
-    print(str(B))
+
     assert B[0, 0] == A[0, 1]
     assert B[0, 1] == A[0, 2]
     assert B[1, 0] == A[1, 1]
@@ -35,11 +36,69 @@ def test_getitem_range():
 def test_getitem_array():
     A = get_test_matrix()
     B = A[[0, 1], [0, 2]]
-    print(str(B))
+
     assert B[0, 0] == A[0, 0]
     assert B[0, 1] == A[0, 2]
     assert B[1, 0] == A[1, 0]
     assert B[1, 1] == A[1, 2]
+
+def test_setitem():
+    A = get_test_matrix()
+
+    B = CrsMatrix(m=A.m, n=A.n)
+    B[0, 0] = 1
+    B[0, 2] = 2
+    B[1, 1] = 3
+    B[2, 1] = 4
+    B[2, 2] = 5
+    B.assemble()
+
+    for i in range(A.m):
+        for j in range(A.n):
+            assert A[i, j] == B[i, j]
+
+def test_setitem_range():
+    A = get_test_matrix()
+
+    B = CrsMatrix(m=5, n=6)
+    B[0:3, 1:4] = A
+    B.assemble()
+
+    assert A.begA[-1] == B.begA[-1]
+    for i in range(A.m):
+        for j in range(A.n):
+            assert A[i, j] == B[i, j + 1]
+
+def test_setitem_array():
+    A = get_test_matrix()
+
+    iidx = [1, 3, 4]
+    jidx = [0, 2, 1]
+
+    B = CrsMatrix(m=5, n=6)
+    B[iidx, jidx] = A
+    B.assemble()
+
+    assert A.begA[-1] == B.begA[-1]
+    for i in range(A.m):
+        for j in range(A.n):
+            assert A[i, j] == B[iidx[i], jidx[j]]
+
+def test_setitem_to_array():
+    A = get_test_matrix()
+    A2 = A.to_dense()
+
+    iidx = [1, 3, 4]
+    jidx = [0, 2, 1]
+
+    B = CrsMatrix(m=5, n=6)
+    B[iidx, jidx] = A2
+    B.assemble()
+
+    assert A.begA[-1] == B.begA[-1]
+    for i in range(A.m):
+        for j in range(A.n):
+            assert A[i, j] == B[iidx[i], jidx[j]]
 
 def test_to_dense():
     A = get_test_matrix()
