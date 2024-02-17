e5089e5deb8358eeb0cb1691fdc89f8d52b8fa3e
Sven Baars <s.baars@uu.nl>
2024-02-17 13:36:26 +0100

    tests: Run the HYMLS stretched test as parametrization in test_continuation.
    Note that the original test was wrong and was actually testing 3D.

--- tests/test_HYMLS.py
+++ tests/test_HYMLS.py
@@ -524,34 +524,3 @@ def test_HYMLS_2D(nx=8):
     x = continuation.continuation(x0, 'Reynolds Number', start, target, ds)[0]
 
     assert x.Norm2() > 0
-
-def test_HYMLS_2D_stretched(nx=8):
-    try:
-        from transiflow.interface import HYMLS as HYMLSInterface
-    except ImportError:
-        pytest.skip("HYMLS not found")
-
-    numpy.random.seed(1234)
-
-    dim = 3
-    dof = 4
-    ny = nx
-    nz = 1
-
-    parameters = {'Reynolds Number': 0,
-                  'Bordered Solver': True,
-                  'Grid Stretching': True,
-                  'Verbose': True}
-
-    interface = HYMLSInterface.Interface(parameters, nx, ny, nz, dim, dof)
-    continuation = Continuation(interface, parameters)
-
-    x0 = interface.vector()
-    x0 = continuation.newton(x0)
-
-    start = 0
-    target = 2000
-    ds = 100
-    x = continuation.continuation(x0, 'Reynolds Number', start, target, ds)[0]
-
-    assert x.Norm2() > 0
