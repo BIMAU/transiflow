c3939e12fc4d88004b56d27bd021e292596410b7
Lourens Veen <l.veen@esciencecenter.nl>
2024-05-29 21:27:16 +0200

    doc: Add initial documentation build configuration.

---
+++ doc/conf.py
@@ -0,0 +1,63 @@
+import os
+import sys
+from unittest.mock import MagicMock
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TransiFlow'
+copyright = '2024 Utrecht University'
+author = 'Sven Baars'
+release = '0.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Allow apidoc to import the code. External dependencies need to be mocked to
+# make that possible without installing them, which is hard.
+sys.path.insert(0, os.path.abspath('..'))
+
+sys.modules['PyTrilinos'] = MagicMock()
+sys.modules['HYMLS'] = MagicMock()
+sys.modules['jadapy'] = MagicMock()
+sys.modules['jadapy.orthogonalization'] = MagicMock()
+sys.modules['petsc4py'] = MagicMock()
+
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.apidoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+add_module_names = False
+
+apidoc_module_dir = '../transiflow'
+apidoc_output_dir = 'reference'
+apidoc_excluded_paths = [
+    'interface',
+    'interface/**',
+    'BoundaryConditions.py',
+    'Continuation.py',
+    'CrsMatrix.py',
+    'CylindricalDiscretization.py',
+    'TimeIntegration.py',
+    'Discretization.py',
+]
+apidoc_separate_modules = True
+apidoc_toc_file = False
+apidoc_module_first = False
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ doc/index.rst
@@ -0,0 +1,21 @@
+.. TransiFlow documentation master file, created by
+   sphinx-quickstart on Thu May 23 10:44:30 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to TransiFlow's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: API documentation
+
+   reference/transiflow
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
