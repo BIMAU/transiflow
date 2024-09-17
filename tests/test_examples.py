81cb30b497ec8d7ddef1f897faab1ddc3ca328aa
Sven Baars <s.baars@uu.nl>
2024-09-17 13:57:37 +0200

    tests: Automatically mark all example tests as example.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,25 +3,23 @@ import matplotlib
 
 matplotlib.use('Agg')
 
-@pytest.mark.example
+pytestmark = pytest.mark.example
+
 def test_ldc_example():
     from examples.ldc import main
 
     main(nx=4)
 
-@pytest.mark.example
 def test_ldc2_example():
     from examples.ldc2 import main
 
     main(nx=4)
 
-@pytest.mark.example
 def test_ldc3_example():
     from examples.ldc3 import main
 
     main(nx=4)
 
-@pytest.mark.example
 def test_ldc_3d_example():
     try:
         from examples.ldc_3d import main
@@ -30,19 +28,16 @@ def test_ldc_3d_example():
     except ImportError:
         pytest.skip("HYMLS not found")
 
-@pytest.mark.example
 def test_dhc_example():
     from examples.dhc import main
 
     main(nx=16)
 
-@pytest.mark.example
 def test_qg_example():
     from examples.qg import main
 
     main()
 
-@pytest.mark.example
 def test_amoc_example():
     from examples.amoc import main
 
