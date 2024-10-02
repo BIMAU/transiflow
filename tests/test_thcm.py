2895a3e98ce708e61e2b60d6e362a970afddad0c
Sven Baars <s.baars@uu.nl>
2024-10-02 13:52:51 +0200

    tests: Add THCM boundary tests.

--- tests/test_thcm.py
+++ tests/test_thcm.py
@@ -21,8 +21,44 @@ def test_thcm_lin():
 
     mass_matrix = discretization.mass_matrix()
 
-    # TODO:
-    n = B.n
+    for i in range(n):
+        print(i)
+
+        scaling = mass_matrix[i, i] or -mass_matrix[i+1, i+1]
+
+        print('Expected:')
+        print(B.jcoA[B.begA[i]:B.begA[i+1]])
+        print(B.coA[B.begA[i]:B.begA[i+1]])
+
+        print('Got:')
+        print(A.jcoA[A.begA[i]:A.begA[i+1]])
+        print(A.coA[A.begA[i]:A.begA[i+1]])
+
+        print('Scaled:')
+        print(-A.coA[A.begA[i]:A.begA[i+1]] / scaling)
+
+        assert B.begA[i+1] - B.begA[i] == A.begA[i+1] - A.begA[i]
+        for j in range(B.begA[i], B.begA[i+1]):
+            assert B.jcoA[j] == A.jcoA[j]
+            assert B.coA[j] == pytest.approx(-A.coA[j] / scaling)
+
+def test_thcm_bnd():
+    nx = 4
+    ny = nx
+    nz = nx
+    dim = 3
+    dof = 6
+    parameters = {'Problem Type': 'Ocean'}
+    n = nx * ny * nz * dof
+
+    discretization = OceanDiscretization(parameters, nx, ny, nz, dim, dof)
+    atom = discretization.linear_part()
+    discretization.boundaries(atom)
+    A = testutils.assemble_jacobian(atom, nx, ny, nz, dof)
+
+    B = testutils.read_matrix('data/thcm_bnd_%sx%sx%s.txt' % (nx, ny, nz))
+
+    mass_matrix = discretization.mass_matrix()
 
     for i in range(n):
         print(i)
